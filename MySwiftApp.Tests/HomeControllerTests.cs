e65d725e2a0ee726c603ba75c25638d574df6ba3
Azer Engazzou <engazzouazer1@gmail.com>
2025-09-16 22:03:10 +0100

    web and tests project

---
+++ MySwiftApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using my_swift.Controllers;
+using my_swift.Models;
+using my_swift.Services;
+
+namespace MySwiftApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_ReturnsViewWithCategories()
+        {
+            // Arrange
+            var categories = new List<SwiftCategory>
+            {
+                new() { Id = "test", Title = "Test", Messages = new() { new SwiftMessage { Code="MT001", Description="Desc"} } }
+            };
+
+            var serviceMock = new Mock<ISwiftMessageService>();
+            serviceMock.Setup(s => s.GetAllCategories()).Returns(categories);
+
+            var controller = new HomeController(serviceMock.Object);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<List<SwiftCategory>>(viewResult.Model);
+            Assert.Single(model);
+            Assert.Equal("Test", model[0].Title);
+        }
+    }
+}
---
+++ MySwiftApp.Tests/SwiftMessageServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using my_swift.Services;
+
+namespace MySwiftApp.Tests
+{
+    public class SwiftMessageServiceTests
+    {
+        [Fact]
+        public void GetAllCategories_ShouldReturnCategoriesWithMessages()
+        {
+            // Arrange
+            var service = new SwiftMessageService();
+
+            // Act
+            var categories = service.GetAllCategories();
+
+            // Assert
+            Assert.NotNull(categories);
+            Assert.NotEmpty(categories);
+            Assert.All(categories, c => Assert.NotEmpty(c.Messages));
+        }
+    }
+}
